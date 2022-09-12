9afe850f472b4b75b440bece59e0136cc8cea9e0
MateusOFCZ <mateusdacruz357@hotmail.com>
2022-09-12 13:11:00 -0300

    [FIX] ApiKey

---
+++ Project/app/Http/Middleware/ApiKey.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ApiKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $ApiKey = $request->header('x-api-key');
+
+        if(!$ApiKey) abort(401, 'Invalid API Key');
+        return $next($request);
+    }
+}
