a723911c427d7f338fe70af84d72f8ff6d3b6aee
MateusOFCZ <mateusdacruz357@hotmail.com>
2022-09-11 23:10:11 -0300

    Logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,4 +21,9 @@ class LoginController extends Controller
             ]
         ]);
     }
+
+    public function logout()
+    {
+        auth()->user()->currentAccessToken()->delete();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,25 +12,10 @@ use App\Http\Controllers\UserProductController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});*/
-
 Route::prefix('auth')->group(function() {
     Route::post('/login', [LoginController::class, 'login']);
-    Route::post('/logout', [LoginController::class, 'logout']);
     Route::post('/register', [RegisterController::class, 'register']);
+    Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 });
 
 Route::middleware('auth:sanctum')->prefix('company')->group(function() {
