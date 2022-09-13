8b1ad4d64ff2b276e678fdbf585243fa9939e234
MateusOFCZ <mateusdacruz357@hotmail.com>
2022-09-13 09:59:35 -0300

    Update Inverter Letras.js

--- Documents/Inverter Letras.js
+++ Documents/Inverter Letras.js
@@ -1,25 +1,25 @@
 //JavaScript Playground: https://playcode.io/960495
 
 let value = "5LABS: SEMPRE A FRENTE";
-let final = null;
 var word = [];
 var result = [];
+let final = null;
 
-let x = value.length;
+let x = value.length; //22
 let y = 0;
 let z = 0;
 
 //Separa as palavras da frase
 while (y < x) {
 	if (value[y] == " ") {
-    	z++;
-    } else if (word[z] != undefined) {
-    	word[z] = word[z] + value[y]; //Palavra = Palavra + Letra
-    } else {
-    	word[z] = value[y];  //Palavra = Letra
-    }
-    
-    y++;
+  	z++;
+  } else if (word[z] != undefined) {
+  	word[z] = word[z] + value[y]; //Palavra = Palavra + Letra
+  } else {
+  	word[z] = value[y];  //Palavra = Letra
+  }
+  
+  y++;
 }
 
 y = 0;
@@ -28,25 +28,25 @@ y = 0;
 for (const value of word) {
 	x = value.length;
     
-    //Inverte as letras da palavra
+  //Inverte as letras da palavra
 	while (x >= 0) {
-    	if (result[y] != undefined) {
-        	result[y] = result[y] + value[x]; //Palavra = Palavra + Última Letra
-        } else {
-        	result[y] = value[x]; //Palavra = Última Letra
-        }
-
-      	x--;
-  	}
-    
-    //Monta a frase novamente
-    //result = [":SBAL5", "ERPMES", "A", "ETNERF"]
-    if (final != null) {
-    	final = final + " " + result[y];
+	  if (result[y] != undefined) {
+    	result[y] = result[y] + value[x]; //Palavra = Palavra + Última Letra
     } else {
-    	final = result[y];
+    	result[y] = value[x]; //Palavra = Última Letra
     }
-    y++;
+
+    x--;
+  }
+    
+  //Monta a frase novamente
+  //result = [":SBAL5", "ERPMES", "A", "ETNERF"]
+  if (final != null) {
+  	final = final + " " + result[y];
+  } else {
+  	final = result[y];
+  }
+  y++;
 }
 
 console.log(final); //Imprime a frase - :SBAL5 ERPMES A ETNERF
@@ -54,18 +54,18 @@ console.log(final); //Imprime a frase - :SBAL5 ERPMES A ETNERF
 /*----------------------------------------------------------------------*/
 
 value = "5LABS: SEMPRE A FRENTE";
-final = null;
-word = null
 result = [];
+final = null;
+word = null;
 x = 0;
 
 word = value.split(" "); //Separa as palavras da frase - word = ["5LABS:", "SEMPRE", "A", "FRENTE"]
 
 for (const value of word) {
 	word = value.split(""); //Separa as letras da palavra - word = ["5", "L", "A", "B", "S", ":"]
-    result[x] = word.reverse().join(""); //Inverte as letras das palavra - word.reverse() = [":", "S", "B", "A", "L", "5"] - join("") = ":SBAL5"
+  result[x] = word.reverse().join(""); //Inverte as letras das palavra - word.reverse() = [":", "S", "B", "A", "L", "5"] - join("") = ":SBAL5"
 
-    x++;
+  x++;
 }
 
 final = result.join(" "); //Monta a frase novamente - result = [":SBAL5", "ERPMES", "A", "ETNERF"]
